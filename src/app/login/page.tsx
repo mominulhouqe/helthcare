861442f4971299457f8a62dae88282502da0ceaf
Mominul <ulhouqemomin39@gmail.com>
2024-10-23 14:56:30 +0600

    Added react hook login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -14,11 +14,32 @@ import Image from "next/image";
 import assest from "@/assets";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 import { useState } from "react";
-import { theme } from "@/lib/theme/theme";
 import Link from "next/link";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+
+interface IpatientLoginValue {
+  email: string;
+  password: string;
+}
 
 const LoginPage = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<IpatientLoginValue>();
+
+  const onSubmit: SubmitHandler<IpatientLoginValue> = (data) => {
+    console.log(data);
+    try {
+    } catch (err: any) {
+      console.log(err.message);
+    }
+  };
 
   const handleShowPassword = () => {
     setShowPassword((prev) => !prev);
@@ -67,60 +88,66 @@ const LoginPage = () => {
             </Typography>
           </Box>
         </Stack>
-        <Box>
-          <Grid container spacing={2} mt={1}>
-            <Grid item xs={12} md={6}>
-              <TextField
-                id="email"
-                label="Email"
-                variant="outlined"
-                size="small"
-                fullWidth={true}
-              />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Box>
+            <Grid container spacing={2} mt={1}>
+              <Grid item xs={12} md={6}>
+                <TextField
+                  id="email"
+                  label="Email"
+                  variant="outlined"
+                  size="small"
+                  fullWidth={true}
+                  {...register("password")}
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <TextField
+                  id="password"
+                  label="Password"
+                  variant="outlined"
+                  size="small"
+                  fullWidth={true}
+                  {...register("password")}
+                  type={showPassword ? "text" : "password"}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleShowPassword}
+                        >
+                          {showPassword ? <Visibility /> : <VisibilityOff />}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </Grid>
             </Grid>
-            <Grid item xs={12} md={6}>
-              <TextField
-                id="password"
-                label="Password"
-                variant="outlined"
-                size="small"
-                fullWidth={true}
-                type={showPassword ? "text" : "password"}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={handleShowPassword}
-                      >
-                        {showPassword ? <Visibility /> : <VisibilityOff />}
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-              />
-            </Grid>
-          </Grid>
-        </Box>
-        <Box sx={{ textAlign: "left", my: 1, mt: 3 }}>
-          <Typography
-            variant="body2"
-            color="text.secondary"
-            sx={{
-              cursor: "pointer",
-            }}
+          </Box>
+          <Box sx={{ textAlign: "left", my: 1, mt: 3 }}>
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              sx={{
+                cursor: "pointer",
+              }}
+            >
+              Forget your password?
+            </Typography>
+          </Box>
+          <Button
+            variant="contained"
+            color="primary"
+            size="medium"
+            sx={{ borderRadius: 2, my: "15px", width: "82%" }}
+            type="submit"
           >
-            Forget your password?
-          </Typography>
-        </Box>
-        <Button
-          variant="contained"
-          color="primary"
-          size="medium"
-          sx={{ borderRadius: 2, my: "15px", width: "82%" }}
-        >
-          Login
-        </Button>
+            Login
+          </Button>
+        </form>
+
         <Typography variant="body1" color="secondary.main" sx={{ mt: 2 }}>
           Are you new here?{" "}
           <Link href="/register" style={{ color: "blue", fontWeight: 550 }}>
